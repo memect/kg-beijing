90ff9c99f6859ae9391ef8304bd45e3e28050850
xpgeng <xpgeng.me@gmail.com>
2016-06-29 21:58:03 +0800

    could fetch.....

--- class1/week3/homework/xpgeng/postgres.py
+++ class1/week3/homework/xpgeng/postgres.py
@@ -9,4 +9,23 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS
     dish(name text, data jsonb)
     ''')
+
+cursor.execute('''
+    INSERT INTO dish
+    VALUES (%s, %s)
+    ''', ['仰望星空', '{"人数": 2, "时间": "30分钟"}'])
+
+
+cursor.execute('''
+    INSERT INTO dish
+    VALUES (%s, %s)
+    ''', ['发人人', '{"人二二": 4, "时扥": "森达"}'])
+
+
+cursor.execute('''
+    SELECT name, data
+    FROM dish
+    ''')
+
+print cursor.fetchall()
 conn.commit()
