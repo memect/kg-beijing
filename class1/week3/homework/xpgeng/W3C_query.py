69f4a0e4d1c862b414fa1d7914a4d5b788162c41
xpgeng <xpgeng.me@gmail.com>
2016-07-04 21:13:41 +0800

    delete some useless codes

--- class1/week3/homework/xpgeng/W3C_query.py
+++ class1/week3/homework/xpgeng/W3C_query.py
@@ -14,17 +14,6 @@ cursor.execute('''SELECT data FROM W3C
 
 row = cursor.fetchone()
 
-
-## query headers->From
-# name_list = []
-# while row:
-#     dic = row['data']
-#     print dic['headers']['From']
-#     name_list.append(dic['headers']['From'])
-#     row = cursor.fetchone()
-
-# print "共 %d 位" % len(name_list)
-
 print row
 
 cursor.close()
